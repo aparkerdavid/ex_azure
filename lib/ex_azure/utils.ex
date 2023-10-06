da079ad628ec8ff8812eb9eb2796e16660dee1fa
Parker Rueve <parkerrueve@gmail.com>
2023-10-06 15:48:08 -0400

    don't call String.valid? on non-binary terms

--- lib/ex_azure/utils.ex
+++ lib/ex_azure/utils.ex
@@ -1,23 +1,24 @@
 defmodule ExAzure.Utils do
+  def normalize_to_charlist(values) when is_list(values) do
+    for value <- values, do: normalize_to_charlist(value)
+  end
+
   def normalize_to_charlist({k, v}) do
     {normalize_to_charlist(k), normalize_to_charlist(v)}
   end
-  def normalize_to_charlist([h | t]) do
-    [normalize_to_charlist(h) | normalize_to_charlist(t)]
-  end
 
-  def normalize_to_charlist(term) do
-    with \
-      true <- String.valid?(term),
-      charlist <- Kernel.to_charlist(term),
-      true <- ascii_printable?(charlist)
-    do
+  def normalize_to_charlist(term) when is_binary(term) do
+    with true <- String.valid?(term),
+         charlist <- Kernel.to_charlist(term),
+         true <- ascii_printable?(charlist) do
       charlist
     else
       _ ->
         term
     end
   end
+
+  def normalize_to_charlist(term), do: term
 
   defp ascii_printable?(list, counter \\ :infinity)
 
@@ -26,7 +27,7 @@ defmodule ExAzure.Utils do
   end
 
   defp ascii_printable?([char | rest], counter)
-      when is_integer(char) and char >= 32 and char <= 126 do
+       when is_integer(char) and char >= 32 and char <= 126 do
     ascii_printable?(rest, decrement(counter))
   end
 
